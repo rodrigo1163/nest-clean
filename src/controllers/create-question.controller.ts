bd2169a76031fd0990575b25a97942ba0202a4e1
rodrigo1163 <rodrigo.sena1163@gmail.com>
2026-08-26 23:10:20 -0400

    feat(auth): update JWT strategy and add CurrentUser decorator for user payload handling

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { CurrentUser } from '../auth/current-user-decorator'
+import type { UserPayload } from '../auth/jwt.strategy'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
 	@Post()
-	async handle() {}
+	async handle(@CurrentUser() user: UserPayload) {
+		console.log(user)
+	}
 }
