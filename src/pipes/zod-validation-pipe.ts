b619e557ae72033f229f9252347e6fd048e6c97d
rodrigo1163 <rodrigo.sena1163@gmail.com>
2026-08-26 21:09:06 -0400

    feat: add account creation endpoint with validation

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,24 @@
+
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { treeifyError, ZodError, ZodType } from 'zod';
+import { fromError } from 'zod-validation-error';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodType<any, any, any>) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      if(error instanceof ZodError) {
+        throw new BadRequestException({
+          statusCode: 400,
+          message: 'Validation failed',
+          error: fromError(error).details, // Ex: "Validation error: Expected string, received number at 'email'"
+        });
+      }
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
