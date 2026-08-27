e74ab0b53b1177e33854a513b8cb8a285ea72211
rodrigo1163 <rodrigo.sena1163@gmail.com>
2026-08-26 23:39:44 -0400

    feat(questions): add fetch recent questions endpoint with pagination support

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,13 +1,52 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import z from 'zod'
 import { CurrentUser } from '../auth/current-user-decorator'
 import type { UserPayload } from '../auth/jwt.strategy'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { PrismaService } from '../prisma/prisma.service'
+
+const createQuestionBodySchema = z.object({
+	title: z.string(),
+	content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
+	constructor(private prisma: PrismaService) {}
+
 	@Post()
-	async handle(@CurrentUser() user: UserPayload) {
-		console.log(user)
+	async handle(
+		@Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+		@CurrentUser() user: UserPayload,
+	) {
+		const { title, content } = body
+		const { sub: userid } = user
+
+		const slug = this.convertToSlug(title)
+
+		await this.prisma.question.create({
+			data: {
+				title,
+				content,
+				slug,
+				authorId: userid,
+			},
+		})
+	}
+
+	private convertToSlug(title: string): string {
+		return title
+			.normalize('NFD') // Separates letters from accents
+			.replace(/[\u0300-\u036f]/g, '') // Removes accents
+			.toLowerCase()
+			.trim()
+			.replace(/[^a-z0-9]+/g, '-') // Replaces separators with hyphens
+			.replace(/^-+|-+$/g, '') // Removes leading/trailing hyphens
 	}
 }
--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,24 +1,22 @@
-
-import { PipeTransform, BadRequestException } from '@nestjs/common';
-import { treeifyError, ZodError, ZodType } from 'zod';
-import { fromError } from 'zod-validation-error';
+import { BadRequestException, PipeTransform } from '@nestjs/common'
+import { ZodError, ZodType } from 'zod'
+import { fromError } from 'zod-validation-error'
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodType<any, any, any>) {}
+	constructor(private schema: ZodType<any, any, any>) {}
 
-  transform(value: unknown) {
-    try {
-      const parsedValue = this.schema.parse(value);
-      return parsedValue;
-    } catch (error) {
-      if(error instanceof ZodError) {
-        throw new BadRequestException({
-          statusCode: 400,
-          message: 'Validation failed',
-          error: fromError(error).details, // Ex: "Validation error: Expected string, received number at 'email'"
-        });
-      }
-      throw new BadRequestException('Validation failed');
-    }
-  }
+	transform(value: unknown) {
+		try {
+			return this.schema.parse(value)
+		} catch (error) {
+			if (error instanceof ZodError) {
+				throw new BadRequestException({
+					statusCode: 400,
+					message: 'Validation failed',
+					error: fromError(error).details, // Ex: "Validation error: Expected string, received number at 'email'"
+				})
+			}
+			throw new BadRequestException('Validation failed')
+		}
+	}
 }
